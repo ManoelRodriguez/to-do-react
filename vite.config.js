82504ccbecfec6359ef8e9ec18ba62ccda97cf2e
Manoel Rodriguez <manoel.rodriguez98@gmail.com>
2024-11-04 16:42:27 -0300

    Resolvendo bug dependências Tailwind

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
-// https://vitejs.dev/config/
-export default {
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+})
