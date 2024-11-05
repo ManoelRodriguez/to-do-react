62356522cd3d5f72d2c34b93683ed9a5cfc1f463
Manoel Rodriguez <manoel.rodriguez98@gmail.com>
2024-11-05 16:47:50 -0300

    Query params

--- src/pages/TaskPage.jsx
+++ src/pages/TaskPage.jsx
@@ -1,6 +1,15 @@
+import { useSearchParams } from "react-router-dom"
+
 function TaskPage() {
-    return(
+
+    const [searchParams] = useSearchParams()
+    const title = searchParams.get("title")
+    const description = searchParams.get("description")
+
+    return (
         <div className="h-screen w-screen bg-slate-500 p-6">
+            <h1>{title}</h1>
+            <h1>{description}</h1>
 
         </div>
     )
